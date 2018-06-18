a3065e244b0bd501065e3f365cf4e36992fc7e3f
Raymond Yu <muddalum@hotmail.com>
2018-06-17 22:20:38 -0700

    Migrated models to database

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+# set up Job class to be ready for database
+class Job(models.Model) :
+    image = models.ImageField(upload_to = 'images/')
+    summary = models.CharField(max_length = 200)
